ada905888dd19591359cb1612515021fcfa2496b
trandatnh <trandatnh@yahoo.com>
2014-05-10 10:48:42 +0800

    + tabControl1

--- thoiloanplayer/thoiloanplayer/MainForm.Designer.cs
+++ thoiloanplayer/thoiloanplayer/MainForm.Designer.cs
@@ -37,6 +37,10 @@ namespace thoiloanplayer
 		private void InitializeComponent()
 		{
 			this.wb = new System.Windows.Forms.WebBrowser();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabControl1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// wb
@@ -47,6 +51,52 @@ namespace thoiloanplayer
 			this.wb.Name = "wb";
 			this.wb.Size = new System.Drawing.Size(1034, 702);
 			this.wb.TabIndex = 0;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tabControl1.Location = new System.Drawing.Point(0, 602);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(1034, 100);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(1026, 74);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1034, 702);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.wb);
+			this.Name = "MainForm";
+			this.Text = "Thời Loạn";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.tabControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.wb.TabIndex = 0;
+		
 			// 
 			// MainForm
 			// 
@@ -60,5 +110,8 @@ namespace thoiloanplayer
 			this.ResumeLayout(false);
 		}
 		private System.Windows.Forms.WebBrowser wb;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
 	}
 }

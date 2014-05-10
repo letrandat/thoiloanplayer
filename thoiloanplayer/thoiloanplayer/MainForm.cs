bdad62f489a1e065edb06698a7c4b79c7069b36c
trandatnh <trandatnh@gmail.com>
2014-05-10 10:20:28 +0800

    + init commit thoiloanplayer
    Signed-off-by: trandatnh <trandatnh@yahoo.com>

---
+++ thoiloanplayer/thoiloanplayer/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 10/5/2014
+ * Time: 10:17 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace thoiloanplayer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}

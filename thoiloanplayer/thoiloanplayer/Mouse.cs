1bafce3bd48e759de8851c2e2654fc10108d8cbb
trandatnh <trandatnh@yahoo.com>
2014-05-11 16:16:32 +0800

    + Mouse.cs

---
+++ thoiloanplayer/thoiloanplayer/Mouse.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * Author: Dat - trandatnh@gmail.com
+ * Date: 11/5/2014
+ * Time: 16:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Runtime.InteropServices;
+
+namespace thoiloanplayer
+{
+	/// <summary>
+	/// Description of Mouse.
+	/// </summary>
+	public class Mouse
+	{
+		const int WM_LBUTTONDOWN = 0x201;
+		const int WM_LBUTTONUP = 0x202;
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+		
+		public Mouse()
+		{
+		}
+		public static void click(IntPtr hWnd, int x, int y)
+		{
+			SendMessage(hWnd, WM_LBUTTONDOWN, (IntPtr)1, (IntPtr)((y << 0x10) | x));
+			SendMessage(hWnd, WM_LBUTTONUP, (IntPtr)0, (IntPtr)((y << 0x10) | x));
+		}
+	}
+}

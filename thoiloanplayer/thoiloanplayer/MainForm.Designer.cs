b44ada826a08a3be5250f89e3d626533985dd9be
trandatnh <trandatnh@yahoo.com>
2014-05-13 12:18:12 +0800

    ~ fixed size, no maximize

--- thoiloanplayer/thoiloanplayer/MainForm.Designer.cs
+++ thoiloanplayer/thoiloanplayer/MainForm.Designer.cs
@@ -65,7 +65,7 @@ namespace thoiloanplayer
 			this.wb.MinimumSize = new System.Drawing.Size(20, 20);
 			this.wb.Name = "wb";
 			this.wb.ScriptErrorsSuppressed = true;
-			this.wb.Size = new System.Drawing.Size(1034, 702);
+			this.wb.Size = new System.Drawing.Size(1034, 701);
 			this.wb.TabIndex = 0;
 			this.wb.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WbDocumentCompleted);
 			// 
@@ -74,7 +74,7 @@ namespace thoiloanplayer
 			this.tabControl1.Controls.Add(this.tabPage1);
 			this.tabControl1.Controls.Add(this.tabPage2);
 			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.tabControl1.Location = new System.Drawing.Point(0, 602);
+			this.tabControl1.Location = new System.Drawing.Point(0, 601);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
 			this.tabControl1.Size = new System.Drawing.Size(1034, 100);
@@ -194,7 +194,7 @@ namespace thoiloanplayer
 			this.pictureBox_show.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.pictureBox_show.BackColor = System.Drawing.Color.Transparent;
 			this.pictureBox_show.Image = global::thoiloanplayer.Resource.left;
-			this.pictureBox_show.Location = new System.Drawing.Point(982, 656);
+			this.pictureBox_show.Location = new System.Drawing.Point(982, 655);
 			this.pictureBox_show.Name = "pictureBox_show";
 			this.pictureBox_show.Size = new System.Drawing.Size(31, 33);
 			this.pictureBox_show.TabIndex = 2;
@@ -210,13 +210,14 @@ namespace thoiloanplayer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1034, 702);
+			this.ClientSize = new System.Drawing.Size(1034, 701);
 			this.Controls.Add(this.pictureBox_show);
 			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.wb);
+			this.MaximizeBox = false;
 			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Thời Loạn";
-			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.tabControl1.ResumeLayout(false);
 			this.tabPage1.ResumeLayout(false);
 			this.tabPage1.PerformLayout();
